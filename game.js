5026eb4e5af95cc00d648c270c920b8ec6ae0f00
Alexandre Costa <alede91@gmail.com>
2023-08-01 16:30:26 -0300

    Functions computerPlay and playRound done

--- game.js
+++ game.js
@@ -1,1 +1,31 @@
 console.log("Rock Paper Scissors Game!");
+
+function computerPlay(){
+    const options = ['rock', 'paper', 'scissors'];
+    const randomValue = Math.floor(Math.random() * options.length);
+
+    return options[randomValue];
+
+};
+
+const computerSelection = computerPlay();
+const playerSelection = 'Rock';
+
+
+function playRound(playerSelection = 'Select a valid value: (Rock, Paper or Scissors)', computerSelection = 'Computer error'){
+    playerSelection = playerSelection.toLowerCase();
+
+    console.log(`Player choice: ${playerSelection}.
+computer choice: ${computerSelection}.`)
+
+    if((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'paper' && playerSelection ==='rock') || (computerSelection === 'scissors' && playerSelection === "paper")){
+        return `Computer Wins! ${computerSelection} defeats ${playerSelection}! Try again!`
+      } else if((computerSelection === 'rock' && playerSelection === 'paper') || (computerSelection === 'paper' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'rock')){
+        return `Player wins ${playerSelection} defeats ${computerSelection}! Congratulations!`
+      } else if (computerSelection === playerSelection){
+        return `That's a tie!`;
+      }
+
+};
+
+console.log(playRound(playerSelection, computerSelection))
