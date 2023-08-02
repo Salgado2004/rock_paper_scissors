fe785ca6cc7a609a570ec1aaaa1f685134a1987a
Alexandre Costa <alede91@gmail.com>
2023-08-02 13:01:41 -0300

    Game is usable, next we need to think ways to crash it to solve possible problems

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 console.log("Rock Paper Scissors Game!");
 
 function computerPlay() {
+
     const options = ['rock', 'paper', 'scissors'];
     const randomValue = Math.floor(Math.random() * options.length);
 
@@ -8,10 +9,24 @@ function computerPlay() {
 
 };
 
-function playRound(playerSelection = 'Select a valid value: (Rock, Paper or Scissors)', computerSelection = 'Computer error') {
-    playerSelection = playerSelection.toLowerCase();
+function getPlayerOption() {
 
-    console.log(`Player choice: ${playerSelection} VS computer choice: ${computerSelection}.`)
+    let playerChoice = prompt('Choose between: Rock, Paper or Scissors!');
+    playerChoice = playerChoice.toLowerCase();
+
+    while (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
+        alert('Your choice must be one of the tree options: Rock, Paper or Scissors!');
+        playerChoice = prompt('Choose between: Rock, Paper or Scissor');
+        playerChoice = playerChoice.toLowerCase();
+    };
+
+    return playerChoice;
+
+};
+
+function playRound(playerSelection, computerSelection) {
+
+    console.log(`Player choice: ${playerSelection} VS computer choice: ${computerSelection}.`);
 
     if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'paper' && playerSelection === 'rock') || (computerSelection === 'scissors' && playerSelection === "paper")) {
         return `Computer Wins the round! ${computerSelection} defeats ${playerSelection}!`;
@@ -21,29 +36,31 @@ function playRound(playerSelection = 'Select a valid value: (Rock, Paper or Scis
         return `That's a tie!`;
     } else {
         return 'Reload';
-    }
+    };
 
 };
 
 function game() {
+
     let playerScore = 0;
     let computerScore = 0;
 
-
     for (let i = 0; i < 5; i++) {
         const computerSelection = computerPlay();
-        const playerSelection = 'Rock';
+        const playerSelection = getPlayerOption();
         const winner = playRound(playerSelection, computerSelection);
-        console.log(winner)
+        console.log(winner);
 
         if (winner.includes('Player')) {
             playerScore = playerScore + 1;
         } else if (winner.includes('Computer')) {
             computerScore = computerScore + 1;
         };
+
         console.log(`Score: Player = ${playerScore} | Computer: ${computerScore}`);
-    }
-    switch(true){
+    };
+
+    switch (true) {
         case computerScore > playerScore:
             console.log('Computer won the game. Try again!');
             break;
@@ -52,7 +69,7 @@ function game() {
             break;
         default:
             console.log('The game tied! Let\'s play again!');
-    }
+    };
 
 };
 
